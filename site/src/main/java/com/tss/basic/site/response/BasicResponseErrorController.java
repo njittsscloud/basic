1a0ec82eccb25104f4892d4ca7c35705d3d2bfb7
moqinggen <mo1056610188@163.com>
2018-11-23 16:51:13 +0800

    处理业务异常

--- site/src/main/java/com/tss/basic/site/response/BasicResponseErrorController.java
+++ site/src/main/java/com/tss/basic/site/response/BasicResponseErrorController.java
@@ -1,6 +1,7 @@
 package com.tss.basic.site.response;
 
-import com.tss.basic.site.response.ErrorDataResponse;
+import com.tss.basic.site.exception.AbstractBusinessException;
+import com.tss.basic.site.exception.ValidationInvalidParamException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
@@ -11,8 +12,11 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -36,15 +40,24 @@ public class BasicResponseErrorController implements ErrorController {
 
     @RequestMapping(value = "${error.path:/error}")
     public ResponseEntity<ErrorDataResponse> error(HttpServletRequest request) {
-//        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-//        Throwable exception = this.errorAttributes.getError(requestAttributes);
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        Throwable exception = this.errorAttributes.getError(requestAttributes);
+
+        HttpStatus status;
         ErrorDataResponse response = new ErrorDataResponse();
         response.setSuccess(false);
-        HttpStatus status = this.getStatus(request);
-        response.setErrorCode(ErrorDataResponse.UNKNOWN_CODE);
-        response.setMsg(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        if (exception instanceof AbstractBusinessException) {
+            AbstractBusinessException e = (AbstractBusinessException) exception;
+            status = HttpStatus.OK;
+            response.setErrorCode(e.getCode());
+            response.setMsg((exception instanceof ValidationInvalidParamException) ? getErrorMessageFromData(e.getData(), e.getMessage()) : e.getMessage());
+            response.setErrorData(e.getData());
+        } else {
+            status = this.getStatus(request);
+            response.setErrorCode(ErrorDataResponse.UNKNOWN_CODE);
+            response.setMsg(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        }
         return new ResponseEntity<>(response, status);
-
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
@@ -58,4 +71,16 @@ public class BasicResponseErrorController implements ErrorController {
         return HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
+    private String getErrorMessageFromData(Object data, String errorMessageDefault) {
+        try {
+            if (data != null && data instanceof Map && ((Map) data).size() > 0) {
+                Object firstValue = ((Map) data).values().stream().findFirst().get();
+                if (firstValue instanceof String) {
+                    return (String) firstValue;
+                }
+            }
+        } catch (Exception e) {
+        }
+        return errorMessageDefault;
+    }
 }
